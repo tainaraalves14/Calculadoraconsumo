03cd4ca65eafa33fa392711a0bae3d2880e77599
Tainara Alves <tadnascimento14@gmail.com>
2024-08-21 19:42:14 -0300

    base one

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿float tempo = 0, vm = 0; distancia = 0; litros  = 0;
+﻿float tempo = 0, vm = 0, distancia = 0, litros  = 0;
+
+Console.WriteLine("Entre como o tempo gasto: ");
+tempo = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com a velocidade média");
+vm = float.Parse(Console.ReadLine());
+
+distancia = tempo * vm;
+
+/* Este cálculo asume que o veículo consume 1 litro de combústivel para cada 
+ * 12 quilometros percorridos */
+litros = distancia / 12;
+
+Console.WriteLine("Sua velocidade média fois de" + vm + "Km/h");
+Console.WriteLine("O tempo gasto foi de" + tempo);
+Console.WriteLine("A distancia percorrida foi de" + distancia + "km");
+Console.WriteLine("Foram utilizados" + litros + "litros");
